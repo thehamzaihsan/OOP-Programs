a00d86b8a5fc5a1690ebb7079f73832615a6eea2
Hamza Ihsan <01-134231-051@student.bahria.edu.pk>
2023-11-13 16:38:44 +0500

    Incomplete

---
+++ MatrixMultiplication/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "main.h"
+using namespace std;
+
+// Define a Matrix class with overloaded * operator to 
+// perform matrix multiplication. 
+// Ensure proper handling of dimensions.
+
+int main(){
+
+    return 0;
+}
---
+++ MatrixMultiplication/main.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Matrix
+{
+private:
+    int rows;
+    int columns;
+    vector<vector<int>> matrix = {};
+
+public:
+    Matrix()
+    {
+      
+    }
+    
+};
+
