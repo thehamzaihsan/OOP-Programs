a79e8fed7ae067c643c7af4a82860057eac70a66
Hamza Ihsan <01-134231-051@student.bahria.edu.pk>
2023-11-13 19:19:18 +0500

    Incomplete

---
+++ character-array/PalindromeChecker/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Palindrome
+{
+private:
+    char *str  = new char[];
+public:
+    Palindrome(char s)
+    {
+       
+        
+    }
+};
+
+int main()
+{
+    char arr[20] = "hbhvbfhvbh";
+
+    return 0;
+}
