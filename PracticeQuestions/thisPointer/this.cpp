45227b9ecd30ab256462229928f5670f3cb1ee07
Hamza Ihsan <01-134231-051@student.bahria.edu.pk>
2023-11-13 14:19:22 +0500

    Some Practice done

---
+++ PracticeQuestions/thisPointer/this.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hamza on 11/6/2023.
+//
+
+#include "this.h"
---
+++ PracticeQuestions/thisPointer/this.h
@@ -0,0 +1,16 @@
+//
+// Created by Hamza on 11/6/2023.
+//
+
+#ifndef OOP_PROGRAMS_THIS_H
+#define OOP_PROGRAMS_THIS_H
+
+
+
+class this {
+
+};
+
+
+
+#endif //OOP_PROGRAMS_THIS_H
