c1dc92561da7e9627554d331af89497e49aed4e9
Hamza Ihsan <01-134231-051@student.bahria.edu.pk>
2023-12-23 22:37:01 +0500

    Templates added

---
+++ Templates/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+template < class atype , class btype> 
+
+
+void foo( atype x , btype y ){
+
+	cout << x + y << endl;
+
+}
+
+int main(){
+
+	int a = 8;
+	float y = 2.5;
+
+	foo(a , y);
+	foo(y , a);
+}
